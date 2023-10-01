6f1b7e66313845f335b28a59a30e9a288b6310da
Aditya Chauhan <ascs25835@gmail.com>
2023-10-01 16:12:57 +0530

    total price implemented

--- src/App.js
+++ src/App.js
@@ -12,21 +12,21 @@ constructor () {
           price: 99,
           title: 'Watch',
           qty: 1,
-          img: '',
+          img: 'https://media.istockphoto.com/id/496730484/photo/apple-watch-sport-42mm-silver-aluminum-case-with-white-band.jpg?s=612x612&w=0&k=20&c=RY2MGp4S-OVqAZm1ZCUDhM6KSmfAJ02RU51l4mJa2EA=',
           id: 1
         },
         {
           price: 999,
           title: 'Mobile Phone',
           qty: 10,
-          img: '',
+          img: 'https://www.designinfo.in/wp-content/uploads/2023/01/Apple-iPhone-14-Pro-Mobile-Phone-493177786-i-1-1200Wx1200H-485x485-optimized.jpeg',
           id: 2
         },
         {
           price: 999,
           title: 'Laptop',
           qty: 4,
-          img: '',
+          img: 'https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFjYm9vayUyMHByb3xlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80',
           id: 3
         }
       ]
@@ -80,6 +80,18 @@ constructor () {
     return count;
   }
 
+  getTotalCount = () => {
+    const { products } = this.state;
+
+    let cartTotal = 0;
+
+    products.map((product) => {
+      cartTotal += cartTotal + product.price * product.qty;
+    })
+
+    return cartTotal;
+  }
+
 
   render (){
     const { products } = this.state;
@@ -92,6 +104,9 @@ constructor () {
        onDecreaseQuantity = {this.handleDecreaseQuantity}
        onDeleteProduct={this.handleDeleteProduct}
        />
+       <div style={{fontSize: 20, padding: 20}}>
+        TOTAL : {this.getTotalCount()}
+       </div>
       </div>
     );
   }
