e8b2cdaaa9405fdd319ab0416a695fc99060f23f
Aditya Chauhan <ascs25835@gmail.com>
2023-10-01 16:03:45 +0530

    Navbar implemented

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = (props) => {
             <div style={styles.nav}>
                 <div style={styles.cartIconContainer}>
                     <img style={styles.cartIcon} src="https://www.freeiconspng.com/thumbs/cart-icon/basket-cart-icon-27.png" alt="cart-icon" />
-                    <span style={styles.cartCount}>3</span>
+                    <span style={styles.cartCount}>{props.count}</span>
                 </div>
             </div>
         );
